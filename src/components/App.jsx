91ce17081a479a738ef9ae824ad5a54113a06afa
Manjunatha C <manjunathac@Nagaratnas-MacBook-Pro.local>
2023-03-04 21:11:34 +0530

    Router config, file and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
-const App = () => (
-  <div>
-    App
-  </div>
-);
+import makeStyles from './styles';
+
+import { NavBar, MovieInformation, Movies, Profile, Actors } from '.';
+
+const App = () => {
+  const classes = makeStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
